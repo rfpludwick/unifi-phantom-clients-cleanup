671b2820eb8415e896c5bfbf1b22f5e6e9a10966
Robert F.P. Ludwick <rfpludwick@gmail.com>
2021-04-24 03:30:51 +0000

    Updates for Go 1.16 and VSCode

--- .devcontainer/provision.sh
+++ .devcontainer/provision.sh
@@ -5,15 +5,6 @@ set -e
 wget https://golang.org/dl/go1.16.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.16.linux-amd64.tar.gz
 rm go1.16.linux-amd64.tar.gz
-echo PATH="${PATH}":/usr/local/go/bin > /etc/profile.d/02-golang.sh
-# shellcheck disable=SC1091
-source /etc/profile
-go get -v golang.org/x/tools/gopls
-go get -v github.com/uudashr/gopkgs/v2/cmd/gopkgs
-go get -v github.com/ramya-rao-a/go-outline
-go get -v github.com/go-delve/delve/cmd/dlv
-go get -v golang.org/x/lint/golint
-go get -v golang.org/x/tools/gopls
 
 # Local provisioner?
 if [ -x .devcontainer/provision-local.sh ]; then
